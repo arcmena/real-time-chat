facae36a53f70a2480124c185bdd6f86a21309f2
Marcelo Mena <marcelomena010@gmail.com>
2021-02-08 01:20:28 -0300

    Disable autoComplete at input

--- app/src/components/ui/Input/index.js
+++ app/src/components/ui/Input/index.js
@@ -1,7 +1,12 @@
 const Input = ({ className, ...rest }) => {
   return (
     <div className={`input__wrapper ${className}`}>
-      <input type="text" className="input__field" {...rest} />
+      <input
+        type="text"
+        className="input__field"
+        autoComplete="off"
+        {...rest}
+      />
     </div>
   );
 };
