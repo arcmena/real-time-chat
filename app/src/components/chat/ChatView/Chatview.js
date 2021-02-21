775743aba57373e9c49cd37a9f098ad3a8348754
Marcelo Mena <marcelomena010@gmail.com>
2021-02-21 17:06:16 -0300

    Start using css modules

---
+++ app/src/components/chat/ChatView/Chatview.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useSubscription, useMutation } from "@apollo/client";
+
+import s from "./ChatView.module.css";
+
+import { Subscription, Mutation } from "../../../graphql";
+
+import Messages from "../Messages";
+import MessageRow from "../MessageRow";
+
+const ChatView = () => {
+  const { data } = useSubscription(Subscription.SUBSCRIBE_MESSAGES);
+
+  const [sendMessage] = useMutation(Mutation.SEND_MESSAGE);
+
+  const [formValues, setFormValues] = useState({
+    user: "Arcmena",
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formValues.content === "") return;
+
+    sendMessage({
+      variables: formValues,
+    });
+
+    formValues.content = "";
+  };
+
+  if (!data) return <>Error...</>;
+
+  return (
+    <div className={s.wrapper}>
+      <h1>ArctiChasadast</h1>
+      <div className="chat">
+        <Messages messages={data.messages} user={formValues.user} />
+        <MessageRow
+          user={formValues.user}
+          content={formValues.content}
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChatView;
