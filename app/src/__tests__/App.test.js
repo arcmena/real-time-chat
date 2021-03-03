3b0405beafea5efaccfbe19e5aa3e21e1b772117
Marcelo Mena <marcelomena010@gmail.com>
2021-03-02 21:05:34 -0300

    Adjust describe on app test

--- app/src/__tests__/App.test.js
+++ app/src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-it('renders learn react link', () => {
+it('renders without crashing', () => {
   render(<App />);
 
   expect(screen.getByText(/Hello there!/i)).toBeInTheDocument();
