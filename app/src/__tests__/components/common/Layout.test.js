61828e677b96bb81f1b9a3f1f9e43b7f1935edfb
Marcelo Mena <marcelomena010@gmail.com>
2021-03-02 21:18:35 -0300

    Adjust describe on Layout test

--- app/src/__tests__/components/common/Layout.test.js
+++ app/src/__tests__/components/common/Layout.test.js
@@ -8,7 +8,7 @@ it('should render AuthView when user is undefined', () => {
     expect(getByText(/Hello there!/i)).toBeInTheDocument();
 });
 
-it('should be able to auth', () => {
+it('should close the auth modal when logged in', () => {
     const { getByText, queryByText, getByPlaceholderText } = render(<Layout />);
 
     fireEvent.change(getByPlaceholderText(/Your badass nickname here/), { target: { value: 'Jhon Doe' } })
