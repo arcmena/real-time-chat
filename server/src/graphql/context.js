0ba19b81e6d95c23e847a3ea271ea934a48ddd97
Marcelo Mena <marcelomena010@gmail.com>
2022-02-06 20:06:32 -0300

    refactor: separate prisma client from gql context

--- server/src/graphql/context.js
+++ server/src/graphql/context.js
@@ -1,7 +1,8 @@
-import { PrismaClient } from '@prisma/client'
 import { PubSub } from 'graphql-subscriptions'
 
-const prisma = new PrismaClient()
+import prismaClient from 'prismaClient'
+
+const prisma = prismaClient
 const pubsub = new PubSub()
 
 export const context = {
