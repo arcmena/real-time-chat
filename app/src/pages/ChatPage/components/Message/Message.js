e761ffe90b20b08005e6c693e08fb617d796b7c9
Marcelo Mena <marcelomena010@gmail.com>
2022-02-13 21:23:28 -0300

    refacotr: mine message logic

--- app/src/pages/ChatPage/components/Message/Message.js
+++ app/src/pages/ChatPage/components/Message/Message.js
@@ -1,21 +1,15 @@
-import cn from 'classnames'
-
 import { useAuth } from 'contexts/AuthContext'
 
-import s from './Message.module.css'
 import { MessageContainer, MessageContent } from './Message.styles'
 
 const Message = ({ message: { content, user: messageFrom } }) => {
   const { me } = useAuth()
 
+  const mine = me.id === messageFrom.id
+
   return (
-    <MessageContainer mine={me.id === messageFrom.id}>
-      <MessageContent
-        mine={me.id === messageFrom.id}
-        // className={cn( ? s.mine : s.not_mine)}
-      >
-        {content}
-      </MessageContent>
+    <MessageContainer mine={mine}>
+      <MessageContent mine={mine}>{content}</MessageContent>
     </MessageContainer>
   )
 }
